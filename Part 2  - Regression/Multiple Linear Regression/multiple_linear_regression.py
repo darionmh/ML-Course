a5d2f0a177b588358708ea130bd3a85d83d78de8
darionmh <darion.higgins@jazzhr.com>
2022-06-24 11:46:27 -0500

    update

---
+++ Part 2  - Regression/Multiple Linear Regression/multiple_linear_regression.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('50_Startups.csv')
+x = dataset.iloc[:, :-1].values #independant vars
+y = dataset.iloc[:, -1].values #dependant vars
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+print(regressor.score(x_test, y_test))
+
+y_predict = regressor.predict(x_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_predict.reshape(len(y_predict), 1), y_test.reshape(len(y_test), 1))))
--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -7,4 +7,4 @@ x = dataset.iloc[:, :-1].values #independant vars
 y = dataset.iloc[:, -1].values #dependant vars
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
